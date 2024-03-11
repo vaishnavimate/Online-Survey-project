d6e80f32f13b754e72225a84527e36b1085f3f2e
Vaishnavi Mate <159476791+vaishnavimate@users.noreply.github.com>
2024-03-11 23:59:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.Survey;
+import java.sql.SQLException;
+
+public class Main 
+{
+		public static void main(String args[]) throws SQLException 
+		{
+			Login login = new Login();
+			login.loginView();
+		}
+	}
+
+
